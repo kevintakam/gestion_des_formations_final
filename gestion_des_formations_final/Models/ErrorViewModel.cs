944acf52e925848cc1f0b28f6c9cfb280e84f6ac
takam <takam@DESKTOP-AFK69BH>
2021-08-18 14:17:30 +0200

    Ajoutez des fichiers projet.

---
+++ gestion_des_formations_final/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gestion_des_formations_final.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
